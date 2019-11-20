1ebdd0989e719c0a4ff1585efbd07449ceab0423
Rodrigo Tenorio de Boa Ventura <boaventura19@yahoo.com.br>
2019-11-20 13:29:46 -0800

    Implementacao do Spring Feign

--- loja/src/main/java/br/com/fiap/loja/service/CompraService.java
+++ loja/src/main/java/br/com/fiap/loja/service/CompraService.java
@@ -7,26 +7,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import br.com.fiap.loja.client.FornecedorClient;
 import br.com.fiap.loja.controller.dto.CompraDTO;
 import br.com.fiap.loja.controller.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
 	
-	@Autowired
-	private RestTemplate client;
+//	@Autowired
+//	private RestTemplate client;
+	
+//	@Autowired
+//	private DiscoveryClient eurekaCliente;
 	
 	@Autowired
-	private DiscoveryClient eurekaCliente;
+	private FornecedorClient fornecedorClient;
 
 	public void realizaCompra(CompraDTO compra) {
+		InfoFornecedorDTO info= fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		System.out.println(info.getEndereco());
+		
 		//RestTemplate client = new RestTemplate();
-		ResponseEntity<InfoFornecedorDTO> exchange =
-		client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(), HttpMethod.GET,null,InfoFornecedorDTO.class);
-		System.out.println(exchange.getBody().getEndereco());
+//		ResponseEntity<InfoFornecedorDTO> exchange =
+//		client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(), HttpMethod.GET,null,InfoFornecedorDTO.class);
+//		System.out.println(exchange.getBody().getEndereco());
 	
-		eurekaCliente.getInstances("fornecedor").stream()
-		                                        .forEach(fornecedor-> System.out.println("localhost: "+fornecedor.getPort()));
+//		eurekaCliente.getInstances("fornecedor").stream().forEach(fornecedor-> System.out.println("localhost: "+fornecedor.getPort()));
 	
 	
 	}
