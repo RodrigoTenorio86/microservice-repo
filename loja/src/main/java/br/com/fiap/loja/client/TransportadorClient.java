97a286d3c2b6e0266f18ee34ce1925e21f692bc1
Rodrigo Tenorio de Boa Ventura <boaventura19@yahoo.com.br>
2020-06-14 18:31:42 -0300

    Integração entre o Zuul e o Eureka Server

--- loja/src/main/java/br/com/fiap/loja/client/TransportadorClient.java
+++ loja/src/main/java/br/com/fiap/loja/client/TransportadorClient.java
@@ -2,7 +2,6 @@ package br.com.fiap.loja.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.fiap.loja.controller.dto.InfoEntregaDTO;
 import br.com.fiap.loja.controller.dto.VoucherDTO;
